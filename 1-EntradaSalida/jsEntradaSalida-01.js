81a8e55978919bbb6517f0970c5f87487b5c288d
riveromatiase <riveromatiase@gmail.com>
2022-01-28 23:49:42 -0300

    nuevo cambio test

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola MUNDO VAMOS A CODEAR");
+	alert("ESTO FUNCIONA DE MARAVILLA");
 }
 
